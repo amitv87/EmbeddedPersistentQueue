922013e7c0d112f55b210906955d58e346c2a2e4
amit verma <amitv87@gmail.com>
2018-04-09 17:39:07 +0530

    platform fixes

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
 git submodule deinit -f .
-git submodule init
-git submodule status | awk '{print $2}' | xargs -P3 -n1 git submodule update --depth 1 --recursive
+
+GIT_VER_MIN="1.9.1"
+GIT_VER=$(git --version | awk '{split($0,a," "); print a[3]}')
+
+if [  "$GIT_VER_MIN" = "`echo -e "$GIT_VER_MIN\n$GIT_VER" | sort -d | head -n1`" ]; then
+  git submodule init
+  git submodule status | awk '{print $2}' | xargs -P3 -n1 git submodule update --init --depth 1 --recursive
+else
+  git submodule update --init --recursive
+fi
